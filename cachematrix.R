c1a5c4c37ddeb924b0a4df4325c1e732301f2357
Tatina <ta_cun@mail.ru>
2016-06-19 00:28:12 +0300

    I'm trying to make changes in an appropriate file

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+##These two functions return an inversed matrix for input x
+##adding this value into a cache list.
 
-## Write a short comment describing this function
+## Creates a cache list for input matrix x
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        s<-NULL
+        set<-function(y){
+                x<<-y
+                s<<-NULL
+        }
+        get<-function() x
+        setsolve<-function(sol) s<<-sol
+        getsolve<-function() s
+        list(set=set,get=get,setsolve=setsolve,getsolve=getsolve)
 }
 
 
-## Write a short comment describing this function
+## Finds inversed matrix for a matrix contained in a cache matrix x
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        s<-x$getsolve()
+        if (!is.null(s)) {
+                message("getting cached data")
+                return(s)
+        }
+        data<-x$get()
+        s<-solve(data)
+        x$setsolve(s)
+        s
 }
